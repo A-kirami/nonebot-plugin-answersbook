7801ca613c19573c1d02497bf547978a10ae0001
BioHazard <3482991796@qq.com>
2023-05-20 21:10:56 +0800

    ✨ 支持通过回复消息触发 (#6)
    ---------
    
    Co-authored-by: Akirami <66513481+A-kirami@users.noreply.github.com>

--- nonebot_plugin_answersbook/__init__.py
+++ nonebot_plugin_answersbook/__init__.py
@@ -1,30 +1,47 @@
 ﻿import random
+from collections.abc import Callable
 from pathlib import Path
 
-from nonebot import on_endswith, on_startswith
-from nonebot.adapters.onebot.v11 import GroupMessageEvent
-from nonebot.matcher import Matcher
+from nonebot import on_message
+from nonebot.adapters.onebot.v11 import Message, MessageEvent, MessageSegment
+from nonebot.params import EventPlainText
+from nonebot.plugin import PluginMetadata
+from nonebot.typing import T_State
 
-try:
-    import ujson as json
-except ModuleNotFoundError:
-    import json
+__plugin_meta__ = PluginMetadata(
+    name="答案之书",
+    description="这是一本治愈系的心灵解惑书，它将带给你的不止是生活的指引，还有心灵的慰藉。",
+    usage=("· 翻看答案 <问题>  # 翻看这个问题的答案\n· <回复一条消息> 翻看答案  # 翻看这个问题的答案\n"),
+)
 
-answers_path = Path(__file__).parent / "answersbook.json"
-answers = json.loads(answers_path.read_text("utf-8"))
+answers_path = Path(__file__).parent / "answersbook.txt"
+answers = answers_path.read_text("utf-8").splitlines()
 
-def get_answers():
-    key = random.choice(list(answers))
-    return answers[key]["answer"]
 
-answers_starts = on_startswith("翻看答案")
-answers_ends = on_endswith("翻看答案")
+def startswith_or_endswith(msg: str) -> Callable[[str], bool]:
+    def rule(message: str = EventPlainText()) -> bool:
+        return message.startswith(msg) or message.endswith(msg)
 
-@answers_starts.handle()
-@answers_ends.handle()
-async def answersbook(event: GroupMessageEvent, matcher: Matcher):
-    msg = event.message.extract_plain_text().replace("翻看答案", "")
-    if not msg:
-        await matcher.finish("你想问什么问题呢？")
-    answer = get_answers()
-    await matcher.send(answer, at_sender=True)
+    return rule
+
+
+look_answer = on_message(rule=startswith_or_endswith("翻看答案"))
+
+
+@look_answer.handle()
+async def answersbook(
+    event: MessageEvent, state: T_State, message: str = EventPlainText()
+) -> None:
+    state["user_id"] = event.user_id
+    if event.reply or message.strip() != "翻看答案":
+        state["question"] = True
+
+
+@look_answer.got("question", prompt=Message.template("{user_id:at}你想问什么问题呢？"))
+async def question(event: MessageEvent) -> None:
+    reply_id = event.reply.message_id if event.reply else event.message_id
+    await look_answer.finish(
+        MessageSegment.reply(reply_id)
+        + MessageSegment.at(event.user_id)
+        + MessageSegment.text(random.choice(answers))
+    )
